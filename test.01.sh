92891144785596d193b1219319d0c178052f741c
Fabio Mosti <fabio.mosti@istricesrl.it>
2024-10-31 16:56:09 +0100

    aggiunto script di test

---
+++ test.01.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# avvio il server
+/bin/python3 /home/fabio/glisweb-agent/glisweb-agent.py
+
+# creo il processo
+value=$( curl -X POST http://localhost:5000/getwebcamdoc -H "Content-Type: application/json" -d '{"comando":"test"}' )
+
+# ricavo il valore di process_id dal json
+process_id=$( echo $value | jq '.process_id' )
+
+# faccio un ciclo fimnché nel json di ritorno non trovo lo status completed
+while [ "$status" != "completed" ]; do
+
+    # chiedo lo stato del processo
+    value=$( curl -X POST http://localhost:5000/getwebcamdocname -H "Content-Type: application/json" -d '{"comando":"status","process_id":'$process_id'}' )
+
+    # ricavo lo status dal json
+    status=$( echo $value | jq '.status' )
+
+    # rimuovo le virgolette
+    status=$( echo $status | tr -d '"' )
+
+    # debug
+    echo $status
+
+    # attendo
+    sleep 1
+
+done
